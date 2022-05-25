8dc75a05914e2c395864d272e7ac378c0609caa3
puksanon <kittipit22@hotmail.com>
2022-05-26 05:36:21 +0700

    :wastebasket: remove alert

--- src/app/components/input-content/input-content.component.ts
+++ src/app/components/input-content/input-content.component.ts
@@ -8,16 +8,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class InputContentComponent {
   @Output() updateInputNumber = new EventEmitter<any>();
   constructor() { }
-
   public number_input: number = 0;
-
   public updateNumber(): void {
     if (this.number_input) {
-      if (typeof (this.number_input) === 'number') {
-        this.number_input > 0 ? this.updateInputNumber.emit(parseInt(this.number_input.toString())) : this.number_input = 1
-      } else {
-        window.alert('please input number only!!')
-      }
+      this.number_input > 0 ? this.updateInputNumber.emit(parseInt(this.number_input.toString())) : this.number_input = 1
     }
   }
 }
